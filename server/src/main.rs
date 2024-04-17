a148ec45a4bb7b0c5c550a780391ed0c4b478544
AbrarNitk <abrar.nitk@gmail.com>
2024-04-17 09:24:49 +0530

    read settings from file

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let profile = std::env::var("PROFILE").ok().unwrap_or("dev".to_owned());
+    let settings = match server::settings::Settings::with_file(profile.as_str()) {
+        Ok(s) => s,
+        Err(err) => {
+            eprintln!("error in reading service settings: {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Hello, world!: {}", profile);
 }
